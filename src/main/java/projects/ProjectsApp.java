cac1020a9569f7b5fd4891c38836bbc637bd7f22
arawls00 <austin_rawls@yahoo.com>
2023-05-19 10:57:31 -0500

    pushing week 10

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,7 +1,6 @@
 package projects;
 
 import java.math.BigDecimal;
-import java.sql.Connection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -16,9 +15,12 @@ public class ProjectsApp {
 	
 	private Scanner scanner = new Scanner(System.in); 
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 	// @formatter:off
 			private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project",
+			"2) List projects",
+			"3) Select a project"
 			);
 			// @formatter:on
 	
@@ -43,6 +45,14 @@ public class ProjectsApp {
 							createProject();
 							break;
 							
+						case 2:
+							listProjects();
+							break;
+							
+						case 3:
+							selectProject();
+							break;
+							
 						default:
 							System.out.println("\n" + selection + " is not a valid selection. Try again.");
 						}
@@ -51,6 +61,25 @@ public class ProjectsApp {
 						System.out.println("\n:Error: " + e + " Try again.");
 					}
 				}
+				
+			}
+			private void selectProject() {
+				listProjects();
+				Integer projectId = getIntInput("Enter a project ID to select a project");
+				
+				curProject = null;
+				
+				curProject = projectService.fetchProjectById(projectId);
+								
+			}
+			
+			private void listProjects() {
+				List<Project> projects = projectService.fetchAllProjects();
+					
+				System.out.println("\nProjects:");
+				
+				projects.forEach(project -> System.out.println("   " + project.getProjectId()
+				+ ": " + project.getProjectName()));
 				
 			}
 			private void createProject() {
@@ -126,6 +155,12 @@ public class ProjectsApp {
 				
 				operations.forEach(line -> System.out.println(" " + line));
 				
+				if(Objects.isNull(curProject)) {
+					System.out.println("\nYou are not working with a project.");
+				}
+				else {
+					System.out.println("\nYou are working with project: " + curProject);
+				}
 			}
 
 }
